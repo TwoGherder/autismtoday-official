5ca3b3fa7498aad68943f5239f048d5b918079fe
JCrucillo2 <junnelcrucillo729@outlook.com>
2022-11-07 03:09:54 -0700

    Newsletter progress. Mostly done just need to resolve spacings

--- footer.php
+++ footer.php
@@ -12,9 +12,32 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+		<div class="newsletter-container">
+			<div class="newsletter-content">
+				<h3>"The best of Autism Today Foundation Canada, straight to your inbox"</h3>
+				<form action="mailto:twogheder@gmail.com" method="post" enctype="text/plain" class="newsletter-form">
+					<p>
+						Be the first to know about what to watch, exclusive previews, and updates from the Autism Today Foundation Canada
+					</p>
+	
+					<div class="newsletter-form-control">
+						<label for="fname">Name:</label>
+						<input type="fname" name="fname" id="fname">
+					</div>
+	
+					<div class="newsletter-form-control">
+						<label for="email">Email:</label>
+						<input type="email" name="email" id="email">
+					</div>
+	
+					<input type="submit" name="submit" id="submit" class="newsletter-submit" value="subscribe">
+				</form>
+			</div>
+		</div>
+
 		<p>&copy; Copyright 2022 Autism Today Foundation Inc, a 501(c)(3) Not-for-Profit Organization</p>
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	</footer>
+</div>
 
 <?php wp_footer(); ?>
 
