7adf8a5552ce13e3c553823639831bf9a907560b
Takitak421 <ron_michael_321@yahoo.com>
2022-11-13 12:28:39 +0800

    Need for Custom Post Type templates

--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -15,23 +16,20 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
+	<?php if (have_posts()) :
+		while (have_posts()) : the_post();
+			// do things -- display content : the function below will pull the content from the template partial.
+			get_template_part('template-parts/content', 'page');
 		endwhile; // End of the loop.
-		?>
+	else :
+		// send to search page / some other general page with search function, tags, categories, archives,etc..
+		get_template_part('template-parts/content', 'none');
+	endif;
+	?>
 
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_sidebar();
