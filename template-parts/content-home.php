988133a9caa87b1c662381fef16be08c381d40ff
Takitak421 <ron_michael_321@yahoo.com>
2022-11-13 12:28:10 +0800

    Need for Custom Post Type templates

---
+++ template-parts/content-home.php
@@ -0,0 +1,108 @@
+<?php
+
+/***
+ * Template part for displaying content in the page.php
+ * @link https://developer.wordpress.org/theme/basics/templatehierarchy/
+ *
+ * @package autismtoday
+ *
+ */
+?>
+<!-- displays the page/post ID of the page -->
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+  <!-- entry header -->
+  <header>
+    <!-- get the page title and display it -->
+    <?php // the_title('<h1 class="entry-title">', '</h1>');
+    ?>
+  </header>
+  <!-- if you had an image it will display using WPs largest default
+thumbnail sizing (settings in the admin - you can see the sizes) -->
+  <main>
+    <?php echo get_the_post_thumbnail($post->ID, 'large'); ?>
+    <div class="entry-content">
+      <!-- display page or post content -->
+      <?php
+      the_content(); // displays all of the content within the editor in pages in the dashboard
+
+
+
+      ?>
+      <!-- other things you could put in here would be: pagination
+(more used for blog posts), custom posts, anything you need for site. -->
+    </div>
+
+    <!-- featured articles -->
+    <?php
+    $args = array(
+      'post_type' => 'featured_articles',
+      'posts_per_page' => 2,
+      'orderby' => 'date', // order by date
+      'order' => 'desc', // show by last descendant
+    );
+    $articles_query = new WP_Query($args);
+    ?>
+
+
+
+    <section class="feature-articles-section">
+      <!-- <h2 class="services-heading">
+      <?php // the_field('services_heading');
+      ?>
+    </h2> -->
+
+      <!-- <p><?php // the_field('services_body_para');
+              ?></p> --> -->
+
+      <!-- card container  -->
+      <div class="row-featured-articles">
+        <!-- loop throughout services content -->
+        <?php if ($articles_query->have_posts()) :  ?>
+          <!-- loop started -->
+          <?php while ($articles_query->have_posts()) : $articles_query->the_post(); ?>
+            <!-- looped content -->
+            <div class="articles-card">
+              <!-- card header image -->
+              <header>
+                <!-- feature image -->
+                <?php echo get_the_post_thumbnail($post->ID, array(850, 850)); ?>
+              </header>
+
+              <div class="services-title">
+                <a href="<?php the_permalink(); ?>"><?php the_title('<h3>', '</h3>') ?></a>
+              </div>
+            </div>
+            <!-- end services--card -->
+
+          <?php endwhile; ?>
+          <!-- end while loop -->
+          <?php wp_reset_postdata(); ?>
+        <?php else : ?>
+          <!-- send to search page / some other general page with search function, tags, categories, archives,etc.. -->
+          <?php get_template_part('template-parts/content', 'none'); ?>
+          <!--  -->
+        <?php endif; ?>
+
+      </div> <!-- end row-services -->
+
+      <!-- <div class="all-services-btn">
+    <?php
+    // $view_all_btn = get_field('view_all_services');
+    // if ($view_all_btn) :
+    //   $view_all_btn_title = $view_all_btn['title'];
+    //   $view_all_btn_url = $view_all_btn['url'];
+    ?>
+      <a href="<?php // print_r(esc_url($view_all_btn_url));
+                ?>"><?php // print_r(esc_html($view_all_btn_title));
+                    ?></a>
+    <?php // endif
+    ?>
+  </div> -->
+    </section>
+
+  </main>
+</article>
+<footer class="entry-footer">
+
+</footer>
