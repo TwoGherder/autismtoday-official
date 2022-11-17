5b6b5d87b33905441296a38643a98f466471f292
JCrucillo2 <junnelcrucillo729@outlook.com>
2022-11-17 00:15:03 -0700

    Styled 404 page and adjusted some gaps on other pages

--- 404.php
+++ 404.php
@@ -12,47 +12,23 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'autismtoday' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'autismtoday' ); ?></p>
-
+		<section class="error-404 not-found error-404-container">
+				<header class="page-header">
+					<h1 class="page-title error-404-title"><?php esc_html_e( 'Oops! It looks like you\'re lost.', 'autismtoday' ); ?></h1>
+				</header><!-- .page-header -->
+	
+				<div class="page-content error-404-content">
+					<p><?php esc_html_e( 'Sorry, we can\'t find that page. Try the search bar below to find available content or go back to homepage.', 'autismtoday' ); ?></p>
+	
 					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
+						get_search_form();
 					?>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'autismtoday' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$autismtoday_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'autismtoday' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$autismtoday_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+					<div class="error-404-home-container">
+						<a class="error-404-home" href="<?php echo esc_url(home_url('/')); ?>" itemprop="url">Home</a>
+					</div>
+			</div>
+		</section>
 
 	</main><!-- #main -->
 
