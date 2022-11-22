59d862062ae8c44bc61dfb1d4a79ac4dbfc6fe97
Takitak421 <ron_michael_321@yahoo.com>
2022-11-22 11:15:00 +0800

    Channel single blog content

---
+++ single-at_channel.php
@@ -0,0 +1,29 @@
+<?php
+
+/***
+ *	The template for displaying single blog post for at_channel
+ *
+ *   @package testingarea
+ *
+ */
+
+//display header
+get_header(); ?>
+
+<?php if (have_posts()) : ?>
+  <?php while (have_posts()) : the_post(); ?>
+    <div class="single-channel-content">
+      <h1 class="single-channel-title">
+        <?php the_title(); ?>
+      </h1>
+      <?php the_content(); ?>
+    </div>
+
+  <?php endwhile; ?>
+  <!-- end while loop -->
+<?php else : ?>
+  <!-- send to search page / some other general page with search function, tags, categories, archives,etc.. -->
+  <?php get_template_part('template-parts/content', 'none'); ?>
+<?php endif;
+//display footer
+get_footer(); ?>
